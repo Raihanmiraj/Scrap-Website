3cad6cee40536355fd3a93a02b2a911901c8056e
Raihanmiraj <65871430+Raihanmiraj@users.noreply.github.com>
2021-03-19 13:03:29 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_GET['link'])){
+    $u = $_GET['link'];
+    $ch = curl_init($u);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    $newquery = curl_exec($ch);
+      $newquery_get = htmlentities($newquery);
+     
+     echo $newquery_get;
+
+}else{
+    echo "<h1>No Link Has Added</h1>";
+}
+
+
+?>
+ 
